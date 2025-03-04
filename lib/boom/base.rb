4827f316e44dbffa5ceea49d9d17003185660009
Sergiy Parkhuts <serparkhuts17@gmail.com>
2025-03-04 17:51:52 +0200

    create 'Boom::Base'

---
+++ lib/boom/base.rb
@@ -0,0 +1,23 @@
+class Boom::Base
+  BASE_URL = 'https://app.boomnow.com/open_api/v1'.freeze
+
+  def self.authenticate
+    url = URI("#{BASE_URL}/auth/token")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request['Content-Type'] = 'application/json'
+    request['Accept'] = 'application/json'
+    request.body = { client_id: ENV['CLIENT_ID'], client_secret: ENV['CLIENT_SECRET'] }.to_json
+
+    response = http.request(request)
+
+    if response.is_a?(Net::HTTPSuccess)
+      JSON.parse(response.body)['access_token']
+    else
+      raise "Authentication failed: #{response.code} - #{response.message} - #{response.body}"
+    end
+  end
+end
