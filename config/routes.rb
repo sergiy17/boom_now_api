dd25e795d0020e62aa50b87c174a452ccb7f65d0
Sergiy Parkhuts <serparkhuts17@gmail.com>
2025-03-04 17:46:46 +0200

    Add routes for listings 'search'

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api do # /api/data
-
-    get '/data', to: 'tests#index'
-    
-    resources :dogs
-
-  end
-
-  get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
+  Rails.application.routes.draw do
+    namespace :api do
+      resources :listings, only: [] do
+        collection do
+          get 'search'
+        end
+      end
+    end
   end
 
 end
